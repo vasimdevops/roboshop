9c83ded4e141a442ac52f10b3e082c9e1a80e825
EC2 Default User <ec2-user@ip-172-31-20-44.ec2.internal>
2021-09-04 07:50:38 +0000

    updated mongodb file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -12,8 +12,8 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
  yum install -y mongodb-org 
  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
  
- #systemctl enable mongod
-# systemctl restart mongod
+ systemctl enable mongod
+systemctl restart mongod
 #Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 #Config file: /etc/mongod.conf
 
